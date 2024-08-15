5c683979eaeffbf537158c8810f21f6bb352ba1b
Desmond Ng <dnyy@Desmonds-MacBook-Pro.local>
2024-08-15 11:08:28 +0800

    Email validations

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,8 +22,22 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use claims::assert_err;
+    use fake::{faker::internet::en::SafeEmail, Fake};
+    use rand::{rngs::StdRng, SeedableRng};
 
     use crate::domain::SubscriberEmail;
+
+    // Both `Clone` and `Debug` are required by `quickcheck`
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -41,5 +55,10 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@gmail.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
